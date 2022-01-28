0fddb39692f4aa02d9b09c73f1a815208adc19c5
Luis Emilio <tdtxle@gmail.com>
2022-01-28 14:59:04 -0600

    ejemplo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,95 +2,109 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:native_pdf_renderer/native_pdf_renderer.dart';
 import 'package:open_as_default/open_as_default.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _path = 'Unknown';
+  File? _file;
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String path;
-    File? file;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      file = await OpenAsDefault.getFileIntent;
-      path = file?.path ?? 'Unknown File';
-      loaderPortada(file?.path ?? "");
-    } on PlatformException {
-      path = 'Failed to get FIle.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _path = path;
-    });
-  }
-
-  Uint8List? _lista;
-  // utilizanod el paque uri_to_file para comvertir el URI en un file
-  // tambien en esta ocacion usaremos el paquete native_pdf_renderer para mostar el pdf
-  loaderPortada(String path) async {
-    final document = await PdfDocument.openFile(path);
-    final page = await document.getPage(1);
-    final image = await page.render(width: page.width, height: page.height);
-    await page.close();
-
-    setState(() {
-      _lista = image!.bytes;
+    OpenAsDefault.getFileIntent.then((value) {
+      if (value != null) {
+        setState(() {
+          _file = value;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(_path),
-              if (_lista != null)
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.memory(
-                        _lista!,
-                        fit: BoxFit.fill,
-                      )),
-                )
-            ],
-          ),
+      title: 'Material App',
+      home: (_file == null) ? PaginaUno() : PaginaDos(path: _file!.path),
+    );
+  }
+}
+
+class PaginaUno extends StatelessWidget {
+  const PaginaUno({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina Uno'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Pagina Uno'),
         ),
       ),
     );
+  }
+}
+
+class PaginaDos extends StatefulWidget {
+  final String path;
+  const PaginaDos({Key? key, required this.path}) : super(key: key);
+
+  @override
+  State<PaginaDos> createState() => _PaginaDosState();
+}
+
+class _PaginaDosState extends State<PaginaDos> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loaderPortada();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina Dos'),
+        backgroundColor: Colors.brown,
+      ),
+      body: Column(
+        children: [
+          const Text('Pagina Dos'),
+          (lista == null)
+              ? Container()
+              : Container(
+                  padding: const EdgeInsets.all(20),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.memory(
+                        lista!,
+                        fit: BoxFit.fill,
+                      )),
+                )
+        ],
+      ),
+    );
+  }
+
+  Uint8List? lista;
+
+  loaderPortada() async {
+    final document = await PdfDocument.openFile(widget.path);
+    final page = await document.getPage(1);
+    final image = await page.render(width: page.width, height: page.height);
+    await page.close();
+
+    setState(() {
+      lista = image!.bytes;
+    });
   }
 }
